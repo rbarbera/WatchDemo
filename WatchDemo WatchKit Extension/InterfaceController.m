ce81491522a0d0652d432566213ade31b403100a
Rafa Barberá <rbarberac@gmail.com>
2014-12-08 11:33:49 +0100

    Initial Interface

--- WatchDemo WatchKit Extension/InterfaceController.m
+++ WatchDemo WatchKit Extension/InterfaceController.m
@@ -10,32 +10,19 @@
 
 
 @interface InterfaceController()
+@property (weak, nonatomic) IBOutlet WKInterfaceImage *interfaceImage;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *interfaceLabel;
 
 @end
 
 
 @implementation InterfaceController
 
-- (instancetype)initWithContext:(id)context {
-    self = [super initWithContext:context];
-    if (self){
-        // Initialize variables here.
-        // Configure interface objects here.
-        NSLog(@"%@ initWithContext", self);
-        
-    }
-    return self;
-}
-
 - (void)willActivate {
-    // This method is called when watch view controller is about to be visible to user
-    NSLog(@"%@ will activate", self);
+    [self.interfaceImage setImageNamed:@"invernalia"];
+    [self.interfaceLabel setText:@"Winter is coming"];
 }
 
-- (void)didDeactivate {
-    // This method is called when watch view controller is no longer visible
-    NSLog(@"%@ did deactivate", self);
-}
 
 @end
 
