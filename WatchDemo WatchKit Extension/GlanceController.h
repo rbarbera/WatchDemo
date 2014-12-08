f6b2da4cbc02df1374cf54e57a933aad91690165
Rafa Barberá <rbarberac@gmail.com>
2014-12-08 11:12:44 +0100

    Watch extension added

---
+++ WatchDemo WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  WatchDemo WatchKit Extension
+//
+//  Created by Rafa Barberá on 08/12/14.
+//  Copyright (c) 2014 develapps. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ WatchDemo WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  WatchDemo WatchKit Extension
+//
+//  Created by Rafa Barberá on 08/12/14.
+//  Copyright (c) 2014 develapps. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
