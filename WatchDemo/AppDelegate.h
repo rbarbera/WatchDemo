b79c949cdfd2af160ec0d217f34a9971a4e56b77
Rafa Barberá <rbarberac@gmail.com>
2014-12-08 11:10:45 +0100

    Initial Commit

---
+++ WatchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatchDemo
+//
+//  Created by Rafa Barberá on 08/12/14.
+//  Copyright (c) 2014 develapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WatchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchDemo
+//
+//  Created by Rafa Barberá on 08/12/14.
+//  Copyright (c) 2014 develapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
