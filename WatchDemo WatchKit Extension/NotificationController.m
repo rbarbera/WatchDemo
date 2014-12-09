fa438115530a4715953fd9484448f0ea3f3f3434
Rafa Barberá <rbarberac@gmail.com>
2014-12-09 09:58:15 +0100

    Label and new location

--- WatchDemo WatchKit Extension/NotificationController.m
+++ WatchDemo WatchKit Extension/NotificationController.m
@@ -8,57 +8,38 @@
 
 #import "NotificationController.h"
 
+@import CoreLocation;
+
 
 @interface NotificationController()
 @property (weak, nonatomic) IBOutlet WKInterfaceMap *interfaceMap;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *messageLabel;
 
 @end
 
 
 @implementation NotificationController
 
-- (instancetype)init {
-    self = [super init];
-    if (self){
-        // Initialize variables here.
-        // Configure interface objects here.
-        NSLog(@"%@ init", self);
-        
-    }
-    return self;
-}
-
-- (void)willActivate {
-    // This method is called when watch view controller is about to be visible to user
-    NSLog(@"%@ will activate", self);
-}
-
-- (void)didDeactivate {
-    // This method is called when watch view controller is no longer visible
-    NSLog(@"%@ did deactivate", self);
-}
-
-/*
-- (void)didReceiveLocalNotification:(UILocalNotification *)localNotification withCompletion:(void (^)(WKUserNotificationInterfaceType))completionHandler {
-    // This method is called when a local notification needs to be presented.
-    // Implement it if you use a dynamic notification interface.
-    // Populate your dynamic notification inteface as quickly as possible.
-    //
-    // After populating your dynamic notification interface call the completion block.
-    completionHandler(WKUserNotificationInterfaceTypeCustom);
-}
-*/
-
-/*
 - (void)didReceiveRemoteNotification:(NSDictionary *)remoteNotification withCompletion:(void (^)(WKUserNotificationInterfaceType))completionHandler {
-    // This method is called when a remote notification needs to be presented.
-    // Implement it if you use a dynamic notification interface.
-    // Populate your dynamic notification inteface as quickly as possible.
-    //
-    // After populating your dynamic notification interface call the completion block.
+    
+    NSDictionary *locationData = remoteNotification[@"locationData"];
+    if (!locationData) {
+        completionHandler(WKUserNotificationInterfaceTypeDefault);
+        return;
+    }
+    
+    CLLocationCoordinate2D center = CLLocationCoordinate2DMake([locationData[@"latitude"] floatValue], [locationData[@"longitude"] floatValue]);
+    
+    double mapSize = [locationData[@"size"] doubleValue];
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(center, mapSize, mapSize);
+    
+    [self.interfaceMap setCoordinateRegion:region];
+    [self.interfaceMap addAnnotation:center withPinColor:WKInterfaceMapPinColorGreen];
+    
+    [self.messageLabel setText:locationData[@"name"]];
+    
     completionHandler(WKUserNotificationInterfaceTypeCustom);
 }
-*/
 
 @end
 
